0c953693087bfb04cb580828590746b631d524af
nick-funk <nick_funk@msn.com>
2017-06-03 11:33:49 -0600

    Throw compile error message on program build
    Previously when a compile error occurred on
    some kernel source code, it would simply throw
    an empty exception with no indication as to
    what had occurred during the build of the kernel.
    
    Instead, we pull the build log for each device
    available to the GPU kernel compiler and return
    it in a new exception so the user is notified.

--- Radium/GpuCompute.cs
+++ Radium/GpuCompute.cs
@@ -49,11 +49,32 @@
             this.Context = new ComputeContext(this.platform.Devices, this.properties, null, IntPtr.Zero);
 
             this.program = new ComputeProgram(this.Context, new[] { this.sourceProgram.Source });
-            this.program.Build(null, "-cl-mad-enable", null, IntPtr.Zero);
+
+            try
+            {
+                this.program.Build(null, "-cl-mad-enable", null, IntPtr.Zero);
+            }
+            catch (Exception)
+            {
+                var compileLog = this.GetCompileLog();
+                throw new Exception(compileLog);
+            }
+
             this.Kernel = this.program.CreateKernel(this.sourceProgram.Name);
 
             this.Commands = new ComputeCommandQueue(this.Context, this.Context.Devices[0], ComputeCommandQueueFlags.None);
             this.Events = new ComputeEventList();
+        }
+
+        private string GetCompileLog()
+        {
+            var compileLog = string.Empty;
+            foreach (var device in this.platform.Devices)
+            {
+                compileLog += this.program.GetBuildLog(device);
+            }
+
+            return compileLog;
         }
     }
 }
